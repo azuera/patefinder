32f7088e3e5d0e106c1b0ddd94ac5a3a31d7d9af
Vlndo <ernaudpiels@icloud.com>
2023-05-02 11:19:33 +0200

    id + label

--- src/View/login.php
+++ src/View/login.php
@@ -2,14 +2,14 @@
 
 var_dump($_POST);
 if (!empty($_POST)) {
-    $usermail = $_POST['email'];
+    $usermail = $_POST['email_username'];
     $sql = "SELECT userr_password FROM userr WHERE userr_email='$usermail' OR userr_name='$usermail'";
     $statement = $connection->query($sql);
     $result = $statement->fetch();
     $hash = $result;
     var_dump($result);
     if (password_verify($_POST['password'], $hash['userr_password'])) {
-        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['email_username'] = $_POST['email_username'];
         echo "Le mot de passe est correct.";
         header('location:index.php?login=success');
     } else {
@@ -21,8 +21,8 @@ if (!empty($_POST)) {
 ?>
 <form action="" method="post">
     <div class="mb-3">
-        <label for="email" class="form-label">usermail/ username</label>
-        <input type="text" class="form-control" id="email" name="email">
+        <label for="email_username" class="form-label">usermail/ username</label>
+        <input type="text" class="form-control" id="email_username" name="email_username">
     </div>
     <div class="mb-3">
         <label for="password" class="form-label">Password</label>
