e2a94943fa7aefad18f9176973280d589d5d9d20
Hicham Benzaied <hichambenzaied69150@gmail.com>
2023-05-16 10:09:21 +0200

    Ajout-bouton-delete-interface-partie

--- src/View/listGame.php
+++ src/View/listGame.php
@@ -18,8 +18,8 @@ foreach ($resultsGame as $resultGame){ ?>
     <div class="card-body">
         <h5 class="card-title"><?= $resultGame->getGameName() ;?></h5>
 
-        <a href="?page=game&index=<?php echo $resultGame->getGameId();?>" class="btn btn-primary">voir détails</a>
-        <a href="?page=deleteGame&index=<?= $resultGame->getGameId();?>" class="btn btn-danger">Supprimer la game</a>
+        <a href="?page=game&index=<?php echo $resultGame->getGameId();?>" class="btn btn-primary">Voir détails</a>
+        <a href="?page=deleteGame&index=<?= $resultGame->getGameId();?>" class="btn btn-danger">Supprimer la partie</a>
     </div>
 </div>
 <?php } ?>
