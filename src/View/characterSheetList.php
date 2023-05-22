41ca4ace31e4c1befe89666d3aab00fa6ddda875
Vlndo <ernaudpiels@icloud.com>
2023-05-22 16:11:46 +0200

    mise a jour de linterface pour differencier un joueur d' un MJ#20 page characterSheetList

--- src/View/characterSheetList.php
+++ src/View/characterSheetList.php
@@ -4,71 +4,127 @@ use Model\CharacterSheet;
 use Model\User;
 
 
-
-
-$sqlCharacterSheet = "SELECT * FROM `character_sheet` ";
-$statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
-$statementCharacterSheet->execute();
-$statementCharacterSheet->setFetchMode(PDO::FETCH_CLASS, CharacterSheet::class);
-$results = $statementCharacterSheet->fetchAll();
-
-//$sqlCharacterSheet = "SELECT * FROM `character_sheet`
-//INNER JOIN charactersheetuser ON character_sheet.characterSheetId = charactersheetuser.characterSheetId";
-//$statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
-//$statementCharacterSheet->bindValue(':userId', $_SESSION['user']->getUserrId());
-//$statementCharacterSheet->execute();
-//$statementCharacterSheet->setFetchMode(PDO::FETCH_CLASS, CharacterSheet::class);
-//$results = $statementCharacterSheet->fetchAll();
-// requete a utiliser si un joueur est connecter et pas un MJ
-
-
+// var_dump($_SESSION['user']);
+// var_dump($_SESSION['user']->getUserrRole());
 ?>
 <a class="btn btn-primary" href="?page=createCharacterSheet"> Créer votre fiche de personnage</a>
 <?php
 
+if (isset($_SESSION['user'])) {
+    if ($_SESSION['user']->getUserrRole() == 2) {
 
-foreach ($results as $result) {
-    $sqlCharacterSheet = "SELECT  `userr`.userrName FROM `userr`
-LEFT JOIN charactersheetuser ON charactersheetuser.userrId = userr.userrId
-WHERE charactersheetuser.characterSheetId = :sheet_id
-GROUP BY userr.userrId";
+        $sqlCharacterSheet = "SELECT * FROM `character_sheet`
+        INNER JOIN charactersheetuser ON character_sheet.characterSheetId = charactersheetuser.characterSheetId
+        WHERE charactersheetuser.userrId = :userId ";
+        $statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
+        $statementCharacterSheet->bindValue(':userId', $_SESSION['user']->getUserrId());
+        $statementCharacterSheet->execute();
+        $statementCharacterSheet->setFetchMode(PDO::FETCH_CLASS, CharacterSheet::class);
+        $results = $statementCharacterSheet->fetchAll();
+        // requete a utiliser si un joueur est connecter et pas un MJ
 
-    $statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
+        if (!empty($results)) {
+            foreach ($results as $result) {
+                $sqlCharacterSheet = "SELECT  `userr`.userrName FROM `userr`
+            LEFT JOIN charactersheetuser ON charactersheetuser.userrId = userr.userrId
+            WHERE charactersheetuser.characterSheetId = :sheet_id
+            GROUP BY userr.userrId";
 
-    $statementCharacterSheet->execute([
-            ':sheet_id' => $result->getCharacterSheetId(),
-    ]);
-    $resultsUser = $statementCharacterSheet->fetchAll(PDO::FETCH_COLUMN);
+                $statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
 
-    if (isset($_SESSION['user'])) {
-        ?>
-        <section>
-            <div class="characterName">
-                <h2>Nom du joueur :
-                    <?php echo implode(', ', $resultsUser) ?>
-                </h2>
-            </div>
-            <div class="characterName">
-                <h3>Nom du personnage :
-                    <?php echo $result->getcharacterSheetName() ?>
-                </h3>
-            </div>
-            <div class="characterName">
-                <p>Classe du personnage :
-                    <?php echo $result->getcharacterSheetClass() ?>
-                </p>
-            </div>
-            <div class="characterName">
-                <p>Statut du personnage :
-                    <?php echo $result->getcharacterSheetStatus() ?>
-                </p>
-            </div>
-            <a class="btn btn-primary" href="?page=characterSheet&index=<?= $result->getCharacterSheetId(); ?>">Voir plus</a>
+                $statementCharacterSheet->execute([
+                    ':sheet_id' => $result->getCharacterSheetId(),
+                ]);
+                $resultsUser = $statementCharacterSheet->fetchAll(PDO::FETCH_COLUMN);
 
-            <?php
+                if (isset($_SESSION['user'])) {
+                    ?>
+                    <section>
+                        <div class="characterName">
+                            <h2>Nom du joueur :
+                                <?php echo implode(', ', $resultsUser) ?>
+                            </h2>
+                        </div>
+                        <div class="characterName">
+                            <h3>Nom du personnage :
+                                <?php echo $result->getcharacterSheetName() ?>
+                            </h3>
+                        </div>
+                        <div class="characterName">
+                            <p>Classe du personnage :
+                                <?php echo $result->getcharacterSheetClass() ?>
+                            </p>
+                        </div>
+                        <div class="characterName">
+                            <p>Statut du personnage :
+                                <?php echo $result->getcharacterSheetStatus() ?>
+                            </p>
+                        </div>
+                        <a class="btn btn-primary" href="?page=characterSheet&index=<?= $result->getCharacterSheetId(); ?>">Voir plus</a>
+
+                        <?php
 
 
+                }
+            }
+        } else {
+            ?>
+                <p>Aucune fiche de personnage créée sur ce compte.</p>
+                <?php
+        }
+
+    } else {
+        $sqlCharacterSheet = "SELECT * FROM `character_sheet` ";
+        $statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
+        $statementCharacterSheet->execute();
+        $statementCharacterSheet->setFetchMode(PDO::FETCH_CLASS, CharacterSheet::class);
+        $results = $statementCharacterSheet->fetchAll();
+
+        foreach ($results as $result) {
+            $sqlCharacterSheet = "SELECT  `userr`.userrName FROM `userr`
+        LEFT JOIN charactersheetuser ON charactersheetuser.userrId = userr.userrId
+        WHERE charactersheetuser.characterSheetId = :sheet_id
+        GROUP BY userr.userrId";
+
+            $statementCharacterSheet = $connection->prepare($sqlCharacterSheet);
+
+            $statementCharacterSheet->execute([
+                ':sheet_id' => $result->getCharacterSheetId(),
+            ]);
+            $resultsUser = $statementCharacterSheet->fetchAll(PDO::FETCH_COLUMN);
+
+            if (isset($_SESSION['user'])) {
+                ?>
+                    <section>
+                        <div class="characterName">
+                            <h2>Nom du joueur :
+                                <?php echo implode(', ', $resultsUser) ?>
+                            </h2>
+                        </div>
+                        <div class="characterName">
+                            <h3>Nom du personnage :
+                                <?php echo $result->getcharacterSheetName() ?>
+                            </h3>
+                        </div>
+                        <div class="characterName">
+                            <p>Classe du personnage :
+                                <?php echo $result->getcharacterSheetClass() ?>
+                            </p>
+                        </div>
+                        <div class="characterName">
+                            <p>Statut du personnage :
+                                <?php echo $result->getcharacterSheetStatus() ?>
+                            </p>
+                        </div>
+                        <a class="btn btn-primary" href="?page=characterSheet&index=<?= $result->getCharacterSheetId(); ?>">Voir
+                            plus</a>
+
+                        <?php
+
+
+            }
+        }
     }
 }
-;
+
 ?>
