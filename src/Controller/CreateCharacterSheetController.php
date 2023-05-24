1b4f258c098fa5e95a904c867cff25bfa9fb7c92
azuera <terminatur45@gmail.com>
2023-05-24 15:14:29 +0200

    ajout des derniere review

--- src/Controller/CreateCharacterSheetController.php
+++ src/Controller/CreateCharacterSheetController.php
@@ -10,8 +10,8 @@ class CreateCharacterSheetController extends AbstractController
 
     public function getContent(): array
     {
-        $characterSheetStatusList=CharacterSheet::CHARACTER_SHEET_STATUS_LIST;
-        $id=null;
+        $characterSheetStatusList = CharacterSheet::CHARACTER_SHEET_STATUS_LIST;
+        $id = null;
         $errors = [];
         $userId = ($_SESSION['user'])->getUserrId();
         $characterSheet = (new CharacterSheet($_POST))
@@ -19,87 +19,70 @@ class CreateCharacterSheetController extends AbstractController
 
         if (!empty($_POST)) {
 
-            $characterSheetName = $_POST["characterSheetName"];
-            $characterSheetClass = $_POST["characterSheetClass"];
-            $characterSheetRace = $_POST["characterSheetRace"];
-            $characterSheetStatus = $_POST["characterSheetStatus"];
 
-            $characteristicInitiative = $_POST["characteristicInitiative"];
-            $characteristicHpMax = $_POST["characteristicHpMax"];
-            $characteristicActualHp = $_POST["characteristicActualHp"];
-            $characteristicMpMax = $_POST["characteristicMpMax"];
-            $characteristicActualMp = $_POST["characteristicActualMp"];
-            $characteristicStrength = $_POST["characteristicStrength"];
-            $characteristicDexterity = $_POST["characteristicDexterity"];
-            $characteristicStamina = $_POST["characteristicStamina"];
-            $characteristicIntelligence = $_POST["characteristicIntelligence"];
-            $characteristicWisdom = $_POST["characteristicWisdom"];
-            $characteristicLuck = $_POST["characteristicLuck"];
-
-            if (empty($characterSheetName)) {
+            if ($characterSheet->getcharacterSheetName() == "") {
                 $errors[] = "Entrer un nom de personnage";
             }
-            if (empty($characterSheetClass)) {
+            if ($characterSheet->getcharacterSheetClass() == "") {
                 $errors[] = "Entrer un nom de classe";
             }
-            if (empty($characterSheetRace)) {
+            if ($characterSheet->getcharacterSheetRace() == "") {
                 $errors[] = "Entrer un nom de race";
             }
 
-            if (!isset($characteristicInitiative) || ($characteristicInitiative < 0 || $characteristicInitiative > 10)) {
+            if ($characterSheet->getcharacteristicInitiative() == 0 || ($characterSheet->getcharacteristicInitiative() < 0 || $characterSheet->getcharacteristicInitiative() > 10)) {
                 $errors[] = "Statistique Initiative doit être comprise entre 0 et 10";
             }
-            if (!isset($characteristicHpMax) || $characteristicHpMax < 1) {
+            if ($characterSheet->getcharacteristicHpMax() == 0 || $characterSheet->getcharacteristicHpMax() < 1) {
                 $errors[] = "Statistique PV Max doit être au minimum de 1";
             }
-            if (!isset($characteristicActualHp) || ($characteristicActualHp < 0 || $characteristicActualHp > $characteristicHpMax)) {
+            if ($characterSheet->getcharacteristicActualHp() == 0 || ($characterSheet->getcharacteristicActualHp() < 0 || $characterSheet->getcharacteristicActualHp() > $characterSheet->getcharacteristicHpMax())) {
                 $errors[] = "Statistique PV Actuel doit être comprise entre 1 et la statistique PV max";
             }
-            if (!isset($characteristicMpMax) || $characteristicMpMax < 0) {
+            if ($characterSheet->getcharacteristicHpMax() == 0 || $characterSheet->getcharacteristicHpMax() < 0) {
                 $errors[] = "Statistique PM Max doit être au minimum de 0";
             }
-            if (!isset($characteristicActualMp) || ($characteristicActualMp < 0 || $characteristicActualMp > $characteristicMpMax)) {
+            if ($characterSheet->getcharacteristicActualHp() == 0 || ($characterSheet->getcharacteristicActualMp() < 0 || $characterSheet->getcharacteristicActualMp() > $characterSheet->getcharacteristicMpMax())) {
                 $errors[] = "Statistique PM Actuel doit être comprise entre 1 et la statistique PM max";
             }
-            if (!isset($characteristicStrength) || ($characteristicStrength < 0 || $characteristicStrength > 20)) {
+            if ($characterSheet->getcharacteristicStrength() == 0 || ($characterSheet->getcharacteristicStrength() < 0 || $characterSheet->getcharacteristicStrength() > 20)) {
                 $errors[] = "Statistique Force doit être comprise entre 0 et 20";
             }
-            if (!isset($characteristicDexterity) || ($characteristicDexterity < 0 || $characteristicDexterity > 20)) {
+            if ($characterSheet->getcharacteristicDexterity() == 0 || ($characterSheet->getcharacteristicDexterity() < 0 || $characterSheet->getcharacteristicDexterity() > 20)) {
                 $errors[] = "Statistique Dexterité doit être comprise entre 0 et 20";
             }
-            if (!isset($characteristicStamina) || ($characteristicStamina < 0 || $characteristicStamina > 20)) {
+            if ($characterSheet->getcharacteristicStamina() == 0 || ($characterSheet->getcharacteristicStamina() < 0 || $characterSheet->getcharacteristicStamina() > 20)) {
                 $errors[] = "Statistique Constitution doit être comprise entre 0 et 20";
             }
-            if (!isset($characteristicIntelligence) || ($characteristicIntelligence < 0 || $characteristicIntelligence > 20)) {
+            if ($characterSheet->getcharacteristicIntelligence() == 0 || ($characterSheet->getcharacteristicIntelligence() < 0 || $characterSheet->getcharacteristicIntelligence() > 20)) {
                 $errors[] = "Statistique Intelligence doit être comprise entre 0 et 20";
             }
-            if (!isset($characteristicWisdom) || ($characteristicWisdom < 0 || $characteristicWisdom > 20)) {
+            if ($characterSheet->getcharacteristicWisdom() == 0 || ($characterSheet->getcharacteristicWisdom() < 0 || $characterSheet->getcharacteristicWisdom() > 20)) {
                 $errors[] = "Statistique Sagesse doit être comprise entre 0 et 20";
             }
-            if (!isset($characteristicLuck) || $characteristicLuck < 0 || $characteristicLuck > 20) {
+            if ($characterSheet->getcharacteristicLuck() == 0 || $characterSheet->getcharacteristicLuck() < 0 || $characterSheet->getcharacteristicLuck() > 20) {
                 $errors[] = "Statistique Chance doit être comprise entre 0 et 20";
             }
-
 
 
             if (empty($errors)) {
                 $sql = 'INSERT INTO character_sheet (characterSheetName, characterSheetStatus, characterSheetRace, characterSheetClass, characteristicInitiative, characteristicHpMax, characteristicActualHp, characteristicMpMax, characteristicActualMp, characteristicStrength, characteristicDexterity, characteristicStamina, characteristicIntelligence, characteristicWisdom, characteristicLuck) VALUES (:characterSheetName, :characterSheetStatus, :characterSheetRace, :characterSheetClass, :characteristicInitiative, :characteristicHpMax, :characteristicActualHp, :characteristicMpMax, :characteristicActualMp, :characteristicStrength, :characteristicDexterity, :characteristicStamina, :characteristicIntelligence, :characteristicWisdom, :characteristicLuck)';
                 $statement = $this->connection->prepare($sql);
-                $statement->bindValue(':characterSheetName', $characterSheetName, PDO::PARAM_STR);
-                $statement->bindValue(':characterSheetStatus', $characterSheetStatus, PDO::PARAM_STR);
-                $statement->bindValue(':characterSheetRace', $characterSheetRace, PDO::PARAM_STR);
-                $statement->bindValue(':characterSheetClass', $characterSheetClass, PDO::PARAM_STR);
-                $statement->bindValue(':characteristicInitiative', $characteristicInitiative, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicHpMax', $characteristicHpMax, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicActualHp', $characteristicActualHp, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicMpMax', $characteristicMpMax, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicActualMp', $characteristicActualMp, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicStrength', $characteristicStrength, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicDexterity', $characteristicDexterity, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicStamina', $characteristicStamina, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicIntelligence', $characteristicIntelligence, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicWisdom', $characteristicWisdom, PDO::PARAM_INT);
-                $statement->bindValue(':characteristicLuck', $characteristicLuck, PDO::PARAM_INT);
+                $statement->bindValue(':characterSheetName', $characterSheet->getcharacterSheetName(), PDO::PARAM_STR);
+                $statement->bindValue(':characterSheetStatus', $characterSheet->getCharacterSheetStatus(), PDO::PARAM_STR);
+                $statement->bindValue(':characterSheetRace', $characterSheet->getcharacterSheetRace(), PDO::PARAM_STR);
+                $statement->bindValue(':characterSheetClass', $characterSheet->getcharacterSheetClass(), PDO::PARAM_STR);
+                $statement->bindValue(':characteristicInitiative', $characterSheet->getcharacteristicInitiative(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicHpMax', $characterSheet->getCharacteristicHpMax(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicActualHp', $characterSheet->getcharacteristicActualHp(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicMpMax', $characterSheet->getcharacteristicMpMax(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicActualMp', $characterSheet->getcharacteristicActualMp(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicStrength', $characterSheet->getcharacteristicStrength(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicDexterity', $characterSheet->getcharacteristicDexterity(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicStamina', $characterSheet->getcharacteristicStamina(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicIntelligence', $characterSheet->getcharacteristicIntelligence(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicWisdom', $characterSheet->getcharacteristicLuck(), PDO::PARAM_INT);
+                $statement->bindValue(':characteristicLuck', $characterSheet->getcharacteristicLuck(), PDO::PARAM_INT);
                 $statement->execute();
 
 
@@ -112,16 +95,15 @@ class CreateCharacterSheetController extends AbstractController
                 $statementInsertCharacterSheetUser->execute();
 
 
-
                 header("Location: ?page=characterSheet&index=$id");
             }
         }
         return [
-            'id'=>$id,
-            'errors'=>$errors,
-            'userId'=>$userId,
-            'characterSheet'=>$characterSheet,
-            'characterSheetStatusList'=>$characterSheetStatusList,
+            'id' => $id,
+            'errors' => $errors,
+            'userId' => $userId,
+            'characterSheet' => $characterSheet,
+            'characterSheetStatusList' => $characterSheetStatusList,
         ];
     }
 
@@ -132,6 +114,6 @@ class CreateCharacterSheetController extends AbstractController
 
     public function getPageTitle(): string
     {
-       return 'creation de fiche personnage';
+        return 'creation de fiche personnage';
     }
 }
